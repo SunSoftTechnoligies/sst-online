6d20cc21a8148931e8684e55639623b1f33b055b
SunSoftTechnoligies <sunsofttech11@gmail.com>
2024-01-10 14:09:33 +0530

    google login auth changed

--- src/Googlesignup.jsx
+++ src/Googlesignup.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleLogin } from 'react-google-login';
+// import { GoogleLogin } from 'react-google-login';
 
 const responseGoogle = (response) => {
   console.log(response);
@@ -8,7 +8,7 @@ const responseGoogle = (response) => {
 function Googlesignup() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      {/* <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -80,7 +80,7 @@ function Googlesignup() {
           </div>
 
         </form>
-      </div>
+      </div> */}
     </div>
   )
 }
